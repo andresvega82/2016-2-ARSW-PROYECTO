268e1b183db11a1da29c8792fa4e93b695a2ea96
andresvega82 <andresvega82@hotmail.com>
2016-12-09 17:55:20 -0500

    Balanceador de carga

--- src/main/java/edu/eci/arsw/RISKA/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/RISKA/WebSocketConfig.java
@@ -34,14 +34,14 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         //config.enableSimpleBroker("/topic");
-        config.enableStompBrokerRelay("/topic/").setRelayHost("35.165.114.222").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic/").setRelayHost("35.165.114.222:8000").setRelayPort(61613);
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //registry.addEndpoint("/stompendpoint").withSockJS();
-         registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
 
     }
     
