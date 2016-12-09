4fdc965f05f7864249e77197d6c3454edfd8b4b0
andresvega82 <andresvega82@hotmail.com>
2016-12-09 16:23:40 -0500

    static

--- src/main/java/edu/eci/arsw/RISKA/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/RISKA/WebSocketConfig.java
@@ -40,7 +40,8 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/stompendpoint").withSockJS();
+        //registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
         
     }
     
